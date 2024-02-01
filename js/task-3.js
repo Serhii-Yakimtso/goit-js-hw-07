62dd57b50b5711567b49905a733be3c2fb262e70
Serhii-Yakimtso <serafyso@gmail.com>
2024-02-01 23:41:24 +0200

    task 3 (to rework)

---
+++ js/task-3.js
@@ -0,0 +1,21 @@
+const nameInput = document.querySelector('#name-input');
+console.log(nameInput);
+const nameOutput = document.querySelector('#name-output');
+console.log(nameOutput);
+
+nameInput.addEventListener('input', e => {
+  let name = '';
+  const space = '    ';
+  if (e.currentTarget.value !== '') {
+    name = e.currentTarget.value.trim();
+  } else if (
+    //   Доопрацювати
+    e.currentTarget.value.trim() !== space.trim()
+  ) {
+    name = 'Anonymous';
+  } else {
+    name = 'Anonymous';
+  }
+
+  nameOutput.textContent = name;
+});
